40c2d87cb32de00ca97aa100df290565fa77f133
Levi <979646206@qq.com>
2024-06-23 22:05:01 +0800

    添加服务消费类，注册至Nacos服务器

--- nacos-provider-demo/src/main/java/com/myth/mall/cloud/NacosProviderApplication.java
+++ nacos-provider-demo/src/main/java/com/myth/mall/cloud/NacosProviderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NacosDemoApplication {
+public class NacosProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NacosDemoApplication.class, args);
+        SpringApplication.run(NacosProviderApplication.class, args);
     }
 
 }
